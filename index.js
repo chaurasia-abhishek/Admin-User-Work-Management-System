6a2bee68d4a0750974024362fa1a3aaa2f12fdff
Abhishek Chaurasia <achauras2090@gmail.com>
2022-08-06 17:27:15 +0530

    port change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors')
 connetToMongo();
 const app = express();
-const port = 4000;
+const port = 4040;
 app.use(cors())
 app.use(express.json())
 app.get('/', (req, res) => { res.send('hello') })
